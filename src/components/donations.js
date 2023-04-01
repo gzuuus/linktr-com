2f09770ece863151c20b78ccab23ab4264a8946a
gzuuus <gzuuus@protonmail.com>
2023-04-01 16:47:50 +0200

    init

---
+++ src/components/donations.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Donations = () => {
+  return (
+    <div className='donations'>
+      <ul className='dbuttons'>
+        <li><a href="lightning:bitcoinbarcelona@getalby.com "target="_blank"rel="noreferrer">⚡ Donaciones</a></li>
+        <li><a href="https://btcpay.bitcoinbarcelona.xyz/"target="_blank"rel="noreferrer">🔗 Donaciones</a></li>
+      </ul>
+    </div>
+  )
+}
---
+++ src/components/links.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Links = () => {
+  return (
+    <div className='container'>
+      <img src="./img/bbo-logo.png" alt="bbo-logo"></img>
+      <h1>Barcelona Bitcoin Only</h1>
+      <ul>
+        <li><a href="https://bitcoinbarcelona.xyz"target="_blank"rel="noreferrer">🌐 Pagina web</a></li>
+        <li><a href="https://t.me/BitcoinBarcelo2"target="_blank"rel="noreferrer">💬 Telegram</a></li>
+        <li><a href="https://twitter.com/bcnbitcoinonly"target="_blank"rel="noreferrer">🔵 Twitter</a></li>
+        <li><a href="https://snort.social/p/npub10nlyn8wm0da6wqgawnl0qvjx6thyy00mvlpm0aetkx5nf7kw9xdsgz26e8"target="_blank"rel="noreferrer">🟣 Nostr</a></li>
+      </ul>
+    </div>
+  )
+}
