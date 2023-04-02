494c227aba05a6abe3c18563aa432f3fe66667ee
gzuuus <gzuuus@protonmail.com>
2023-04-02 18:00:08 +0200

    typos n stylos

--- src/components/donations.js
+++ src/components/donations.js
@@ -3,9 +3,10 @@ import React from 'react'
 export const Donations = () => {
   return (
     <div className='donations'>
+      <h3>Donaciones</h3>
       <ul className='dbuttons'>
-        <li><a href="lightning:bitcoinbarcelona@getalby.com "target="_blank"rel="noreferrer">⚡ Donaciones</a></li>
-        <li><a href="https://btcpay.bitcoinbarcelona.xyz/"target="_blank"rel="noreferrer">🔗 Donaciones</a></li>
+        <li><a href="lightning:bitcoinbarcelona@getalby.com "target="_blank"rel="noreferrer">⚡ Lightning</a></li>
+        <li><a href="https://btcpay.bitcoinbarcelona.xyz/"target="_blank"rel="noreferrer">🔗 On-chain</a></li>
       </ul>
     </div>
   )
