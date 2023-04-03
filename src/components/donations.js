84a8e873b2edb55c8da26ff7535b7809b23df007
gzuuus <gzuuus@protonmail.com>
2023-04-03 17:30:41 +0200

    rebranded

--- src/components/donations.js
+++ src/components/donations.js
@@ -6,10 +6,10 @@ export const Donations = () => {
       <h3>Donaciones</h3>
       <ul className='dbuttons'>
         <div className='dbuttons inner'>
-          <li><a href="#"target="_blank"rel="noreferrer">⚡ Lightning</a></li>
-          <li><a href="#"target="_blank"rel="noreferrer"><img className='exit-icon' src='./img/fi-br-sign-out.png'></img></a></li>
+          <li><a href="lightning:gzuuus@getalby.com"target="_blank"rel="noreferrer">⚡ Lightning</a></li>
+          <li><a href="https://getalby.com/p/gzuuus"target="_blank"rel="noreferrer"><img className='exit-icon' src='./img/fi-br-sign-out.png' alt=''></img></a></li>
         </div>
-        <li className='dbuttons-last'><a href="#"target="_blank"rel="noreferrer">🔗 On-chain</a></li>
+        <li className='dbuttons-last'><a href="https://btcpay.bitcoinbarcelona.xyz/apps/DGFprW3j8PSTm2FyoS3j1czWHbR/pos"target="_blank"rel="noreferrer">🔗 On-chain</a></li>
       </ul>
     </div>
   )
--- src/components/links.js
+++ src/components/links.js
@@ -3,13 +3,13 @@ import React from 'react'
 export const Links = () => {
   return (
     <div className='container'>
-      <img src="./img/fi-br-sign-out.png" alt="bbo-logo"></img>
+      <img className='header-img' src="./img/gz-pfp.png" alt="bbo-logo"></img>
       <h1>Linktr</h1>
       <ul>
-        <li><a href="#"target="_blank"rel="noreferrer">🌐 Página web</a></li>
-        <li><a href="#"target="_blank"rel="noreferrer">💬 Telegram</a></li>
-        <li><a href="#"target="_blank"rel="noreferrer">🔵 Twitter</a></li>
-        <li><a href="#"target="_blank"rel="noreferrer">🟣 Nostr</a></li>
+        <li><a href="https://github.com/gzuuus"target="_blank"rel="noreferrer">🌐 Página web</a></li>
+        <li><a href="https://t.me/Gzuuus"target="_blank"rel="noreferrer">💬 Telegram</a></li>
+        <li><a href="https://snort.social/p/npub1gzuushllat7pet0ccv9yuhygvc8ldeyhrgxuwg744dn5khnpk3gs3ea5ds"target="_blank"rel="noreferrer">🟣 Nostr</a></li>
+        <li><a href="https://twitter.com/gzuuus"target="_blank"rel="noreferrer">🔵 Twitter</a></li>
       </ul>
     </div>
   )
