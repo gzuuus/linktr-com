2ebf14aba753da11fe03dfe51a462f1cf8eb7126
gzuuus <gzuuus@protonmail.com>
2023-04-03 12:01:49 +0200

    img fix

--- src/components/donations.js
+++ src/components/donations.js
@@ -7,7 +7,7 @@ export const Donations = () => {
       <ul className='dbuttons'>
         <div className='dbuttons inner'>
           <li><a href="lightning:bitcoinbarcelona@getalby.com "target="_blank"rel="noreferrer">⚡ Lightning</a></li>
-          <li><a href="https://getalby.com/p/bitcoinbarcelona "target="_blank"rel="noreferrer"><img className='exit-icon' src='./img/fi-br-sign-out.svg'></img></a></li>
+          <li><a href="https://getalby.com/p/bitcoinbarcelona "target="_blank"rel="noreferrer"><img className='exit-icon' src='./img/fi-br-sign-out.png'></img></a></li>
         </div>
         <li className='dbuttons-last'><a href="https://btcpay.bitcoinbarcelona.xyz/"target="_blank"rel="noreferrer">🔗 On-chain</a></li>
       </ul>
