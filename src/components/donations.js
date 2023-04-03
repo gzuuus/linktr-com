92d038c297aa598eda7fc967131bc7c60f193f07
gzuuus <gzuuus@protonmail.com>
2023-04-03 12:17:19 +0200

    refactor to white label master branch

--- src/components/donations.js
+++ src/components/donations.js
@@ -6,10 +6,10 @@ export const Donations = () => {
       <h3>Donaciones</h3>
       <ul className='dbuttons'>
         <div className='dbuttons inner'>
-          <li><a href="lightning:bitcoinbarcelona@getalby.com "target="_blank"rel="noreferrer">⚡ Lightning</a></li>
-          <li><a href="https://getalby.com/p/bitcoinbarcelona "target="_blank"rel="noreferrer"><img className='exit-icon' src='./img/fi-br-sign-out.png'></img></a></li>
+          <li><a href="#"target="_blank"rel="noreferrer">⚡ Lightning</a></li>
+          <li><a href="#"target="_blank"rel="noreferrer"><img className='exit-icon' src='./img/fi-br-sign-out.png'></img></a></li>
         </div>
-        <li className='dbuttons-last'><a href="https://btcpay.bitcoinbarcelona.xyz/"target="_blank"rel="noreferrer">🔗 On-chain</a></li>
+        <li className='dbuttons-last'><a href="#"target="_blank"rel="noreferrer">🔗 On-chain</a></li>
       </ul>
     </div>
   )
